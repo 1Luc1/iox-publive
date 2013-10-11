1a27391a674a7d342ff2d73415ec578053dfa166
Thorsten Zerha <thorsten.zerha@tastenwerk.com>
2013-10-11 16:56:55 +0200

    archive got default value

--- db/migrate/20130919090000_add_archived_to_venues.rb
+++ db/migrate/20130919090000_add_archived_to_venues.rb
@@ -1,6 +1,6 @@
 class AddArchivedToVenues < ActiveRecord::Migration
   def change
-    add_column :iox_venues, :archived, :boolean
-    add_column :iox_ensembles, :archived, :boolean
+    add_column :iox_venues, :archived, :boolean, default: false
+    add_column :iox_ensembles, :archived, :boolean, default: false
   end
 end
