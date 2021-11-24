d8b72145e07682f82145ee8577f13890051b1ecc
David Reinisch <david@k-os.online>
2021-11-24 16:16:47 +0100

    up;

--- app/helpers/iox/syncers_helper.rb
+++ app/helpers/iox/syncers_helper.rb
@@ -72,7 +72,7 @@ module Iox
           enc_url = URI.escape(image.orig_url)
           enc_url.scheme = "https"
           enc_url.to_s
-          logger.debug "The enc url: #{enc_url}"
+          logger.debug "The enc url: #{enc_ur}l"
           open( enc_url ) do |data|
             file.write data.read
           end
